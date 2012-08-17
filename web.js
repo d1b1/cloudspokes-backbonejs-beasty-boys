9691bcfbd9665a8f6edeb6d4a718165844638ee8
Stephan Smith <steve@soend.com>
2012-08-17 08:39:09 -0400

    Added .gitignore file and heroku requirements

---
+++ web.js
@@ -0,0 +1,29 @@
+var express    = require('express'),
+    requestObj = require('request'),
+    http       = require('http'),
+    crypto     = require('crypto');
+
+// ---------------------------------------------------
+
+var app = express.createServer( express.logger() );
+
+app.configure(function(){
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.cookieParser());
+});
+
+app.configure('production', function() {
+  console.log('Note: release');
+
+  // Paths & Files
+  app.use('/assets/js/libs/', express.static(__dirname + '/public/dist/release/'));
+  app.use('/assets/css/', express.static(__dirname + '/public/dist/release/'));
+});
+
+app.use(express.static( __dirname + '/public/' ));
+
+var port = process.env.PORT || 4200;
+app.listen(port, function() { 
+  console.log("StartUp: backbone.js-geocoder-plugin " + port ); 
+});
